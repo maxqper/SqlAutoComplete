cbfff1c89948f3c3c0801307e8142db5cbda5e7a
maxqper <prashanth.pappu@gmail.com>
2022-08-27 14:19:27 -0400

    Updated example

--- example/main.js
+++ example/main.js
@@ -1,5 +1,5 @@
 import genericAutocompleteParser from '../src/sql/generic/genericAutocompleteParser.js';
 
 const parser = genericAutocompleteParser;
-const result = parser.parseSql('SELECT *', '');
+const result = parser.parseSql('SELECT * F', '');
 console.log(result);
